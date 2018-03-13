17bb1dd443950a38fa5ad28b26fdaafd54f300f9
Daniel Binoy <dbinoy15@gmail.com>
2018-03-12 17:03:48 -0700

    Set up structure
    planned out layout of photoresistors and linear servos. Added diagram. Put down all 4 cases.

--- nedc/nedc.ino
+++ nedc/nedc.ino
@@ -1,12 +1,59 @@
+/*
+ * --------------------------------
+ *                 2
+ *        ____________________
+ *       |fl                fr|
+ *       |                    |
+ *    1  |                    |  3 
+ *       |                    |
+ *       |bl________________br|
+ *                 
+ *                 4
+ * --------------------------------
+ */
+
 #include <Servo.h>
 
+// create all linear servos
+Servo frontRight;
+Servo frontLeft;
+Servo backRight;
+Servo backLeft;
 
 void setup() {
-  // initialize all linear servos
-    
+  // attach all linear servos
+  frontRight.attach(6);
+  frontLeft.attach(9);
+  backRight.attach(10);
+  backLeft.attach(11);    
 }
 
 void loop() {
   // check which side has more light, and turn servos
-
+  if(analogRead(1)<analogRead(3)){
+    if(analogRead(2)<analogRead(4)){
+      frontLeft.write(180);
+      backRight.write(0);
+      frontRight.write(90);
+      backLeft.write(90);
+    }else{
+      frontLeft.write(0);
+      backRight.write(180);
+      frontRight.write(90);
+      backLeft.write(90);
+    }
+  }else{
+    if(analogRead(2)<analogRead(4)){
+      frontLeft.write(180);
+      backRight.write(0);
+      frontRight.write(90);
+      backLeft.write(90);
+    }else{
+      frontLeft.write(90);
+      backRight.write(90);
+      frontRight.write(0);
+      backLeft.write(180);
+    }
+  }
 }
+
